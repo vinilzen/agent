2af25b540c37a2ae4d57bd0a2bb786015a6fffe1
vinilzen <marchukilya@gmail.com>
2013-04-01 22:23:43 +0200

    create Spy Bundle + Point Entity

---
+++ src/Acme/SpyBundle/AcmeSpyBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Acme\SpyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcmeSpyBundle extends Bundle
+{
+}
---
+++ src/Acme/SpyBundle/Form/PointType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Acme\SpyBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PointType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('logo')
+            ->add('coordinates')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Acme\SpyBundle\Entity\Point'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'acme_spybundle_pointtype';
+    }
+}
