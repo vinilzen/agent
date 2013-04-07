9226da7907d9bc1088a0d763b63b0a3033717861
vinilzen <marchukilya@gmail.com>
2013-04-08 01:10:38 +0200

    instal SonataAdminBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,9 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Acme\SpyBundle\AcmeSpyBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\jQueryBundle\SonatajQueryBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
