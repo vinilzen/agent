b08c6fa3ab956e21346e3284678029337a07f9cc
vinilzen <marchukilya@gmail.com>
2013-04-13 12:37:50 +0200

    add Admin Folder

---
+++ src/Acme/SpyBundle/Admin/FranchiseAdmin.php
@@ -0,0 +1,104 @@
+<?php
+namespace Acme\SpyBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Admin\AdminInterface;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+use Knp\Menu\ItemInterface as MenuItemInterface;
+
+class FranchiseAdmin extends Admin
+{
+    /**
+     * Конфигурация отображения записи
+     *
+     * @param \Sonata\AdminBundle\Show\ShowMapper $showMapper
+     * @return void
+     */
+    protected function configureShowField(ShowMapper $showMapper)
+    {
+        $showMapper
+                ->add('id', null, array('label' => 'Идентификатор'))
+                ->add('brand', null, array('label' => 'Бренд'))
+                ->add('industry', null, array('label' => 'Индустрия задания (horeca, cpg, retail)'));
+    }
+
+    /**
+     * Конфигурация формы редактирования записи
+     * @param \Sonata\AdminBundle\Form\FormMapper $formMapper
+     * @return void
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+                ->add('brand', null, array('label' => 'Бренд'))
+                ->add('industry', null, array('label' => 'Индустрия'))
+
+        //by_reference используется для того чтобы при трансформации данных запроса в объект сущности
+        //которую выполняет Symfony Form Framework, использовался setter сущности News::setNewsLinks
+                
+        /*
+                ->add('newsLinks', 'sonata_type_collection',
+                      array('label' => 'Ссылки', 'by_reference' => false),
+                      array(
+                           'edit' => 'inline',
+                           //В сущности NewsLink есть поле pos, отражающее положение ссылки в списке
+                          //указание опции sortable позволяет менять положение ссылок в списке перетаскиваением
+                           'sortable' => 'pos',
+                           'inline' => 'table',
+                      ))
+                ->add('newsCategory', null, array('label' => 'Категория'))*/
+                ->setHelps(array(
+                                'brand' => 'Бренд торговых точки',
+                                'industry' => 'Индустрия задания (horeca, cpg, retail)'
+                           ));
+
+    }
+
+    /**
+     * Конфигурация списка записей
+     *
+     * @param \Sonata\AdminBundle\Datagrid\ListMapper $listMapper
+     * @return void
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+                ->addIdentifier('id')
+                ->addIdentifier('brand', null, array('label' => 'Бренд'))
+                ->add('industry', null, array('label' => 'Индустрия задания'));
+    }
+
+    /**
+     * Поля, по которым производится поиск в списке записей
+     *
+     * @param \Sonata\AdminBundle\Datagrid\DatagridMapper $datagridMapper
+     * @return void
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+                ->add('brand', null, array('label' => 'Бренд'));
+    }
+
+    /**
+     * Конфигурация левого меню при отображении и редатировании записи
+     *
+     * @param \Knp\Menu\ItemInterface $menu
+     * @param $action
+     * @param null|\Sonata\AdminBundle\Admin\AdminInterface $childAdmin
+     * @return void
+     */
+    protected function configureSideMenu(MenuItemInterface $menu, $action, AdminInterface $childAdmin = null)
+    {
+        if ($action == 'edit' || $action == 'show')
+        $menu->addChild(
+            $action == 'edit' ? 'Просмотр сети' : 'Редактирование сети',
+            array('uri' => $this->generateUrl(
+                $action == 'edit' ? 'show' : 'edit', array('id' => $this->getRequest()->get('id'))))
+        );
+    }
+}
---
+++ src/Acme/SpyBundle/Admin/MissionAdmin.php
@@ -0,0 +1,101 @@
+<?php
+namespace Acme\SpyBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Admin\AdminInterface;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+use Knp\Menu\ItemInterface as MenuItemInterface;
+
+class MissionAdmin extends Admin
+{
+    /**
+     * Конфигурация отображения записи
+     *
+     * @param \Sonata\AdminBundle\Show\ShowMapper $showMapper
+     * @return void
+     */
+    protected function configureShowField(ShowMapper $showMapper)
+    {
+        $showMapper
+                ->add('id', null, array('label' => 'Идентификатор'))
+                ->add('runtime', null, array('label' => 'Время выполнения'))
+                ->add('icons', null, array('label' => 'Блок иконок с необходимыми условиями выполнения'))
+                ->add('description', null, array('label' => 'Подсказка по часам работы точки'))
+                ->add('coordinates', null, array('label' => 'Координаты места'))
+                ->add('franchise', null, array('label' => 'Сеть торговых точек'));
+    }
+
+    /**
+     * Конфигурация формы редактирования записи
+     * @param \Sonata\AdminBundle\Form\FormMapper $formMapper
+     * @return void
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+                ->add('title', null, array('label' => 'Название'))
+                ->add('logo', null, array('label' => 'Логотип'))
+                ->add('description', null, array('label' => 'Подсказка по часам работы точки'))
+                ->add('coordinates', null, array('label' => 'Координаты места'))              
+                ->add('franchise', null, array('label' => 'Сеть торговых точек'))
+                ->setHelps(array(
+                                'title' => 'Название торговой точки',
+                                'logo' => 'Путь к картинке логотипу',
+                                'description' => 'Подсказка по часам работы точки',
+                                'coordinates' => 'Координаты места "43.1341313х56.2452t45ty24"',
+                                'franchise' => 'К какой сети относится' 
+                           ));
+
+    }
+
+    /**
+     * Конфигурация списка записей
+     *
+     * @param \Sonata\AdminBundle\Datagrid\ListMapper $listMapper
+     * @return void
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+                ->addIdentifier('id')
+                ->addIdentifier('title', null, array('label' => 'Название'))
+                ->add('logo', null, array('label' => 'Путь к картинке логотипу'))
+                ->add('description', null, array('label' => 'Подсказка по часам работы точки'))
+                ->add('coordinates', null, array('label' => 'Координаты места'))
+                ->add('franchise', null, array('label' => 'Сеть торговых точек'));
+    }
+
+    /**
+     * Поля, по которым производится поиск в списке записей
+     *
+     * @param \Sonata\AdminBundle\Datagrid\DatagridMapper $datagridMapper
+     * @return void
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+                ->add('title', null, array('label' => 'Название'));
+    }
+
+    /**
+     * Конфигурация левого меню при отображении и редатировании записи
+     *
+     * @param \Knp\Menu\ItemInterface $menu
+     * @param $action
+     * @param null|\Sonata\AdminBundle\Admin\AdminInterface $childAdmin
+     * @return void
+     */
+    protected function configureSideMenu(MenuItemInterface $menu, $action, AdminInterface $childAdmin = null)
+    {
+        if ($action == 'edit' || $action == 'show')
+        $menu->addChild(
+            $action == 'edit' ? 'Просмотр точки' : 'Редактирование точки',
+            array('uri' => $this->generateUrl(
+                $action == 'edit' ? 'show' : 'edit', array('id' => $this->getRequest()->get('id'))))
+        );
+    }
+}
