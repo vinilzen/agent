2e9247ed75c8a77e97f83ccb242c2919ecf6117e
vinilzen <marchukilya@gmail.com>
2013-05-01 00:01:57 +0200

    remove points

--- src/Acme/SpyBundle/Controller/MissionController.php
+++ src/Acme/SpyBundle/Controller/MissionController.php
@@ -156,12 +156,7 @@ class MissionController extends Controller
         $entity = new Mission();
         $form   = $this->createForm(new MissionType(), $entity);
 
-        $points = $this->getDoctrine()->getManager()->getRepository('AcmeSpyBundle:Point')->findAll();
-
-        //foreach ($points as $point) {            var_dump($point->getTitle());        }
-
         return array(
-            'points' => $points,
             'entity' => $entity,
             'form'   => $form->createView(),
         );
